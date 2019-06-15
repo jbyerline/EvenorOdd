d6ac7766a5b02a31fb3960bd521906b6361a96e6
Jacob Byerline <47374239+jbyerline@users.noreply.github.com>
2019-06-14 17:15:50 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number: ");
+
+            string sInput = Console.ReadLine();
+
+            int iInput = int.Parse(sInput);
+
+            if (iInput % 2 == 0)
+            {
+                Console.Write(iInput + " is even");
+            }
+            else
+            {
+                Console.Write(iInput + " is odd");
+            }
+        }
+    }
+}
